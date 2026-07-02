b2f9e0057e32b8fe6a880409a375410e22407d8c
Iury <iurysodre22@gmail.com>
2026-07-01 21:52:08 -0300

    feat: add entry point classes for client and server and remove legacy batch utility scripts

--- Cliente/Principal.java
+++ Cliente/Principal.java
@@ -6,7 +6,7 @@
  * Autor: Iury Ramos Sodre
  * Matricula: 202310440
  * Inicio: 15/06/2026
- * Ultima alteracao: 26/06/2026
+ * Ultima alteracao: 01/07/2026
  * Nome: Sistema de Comunicacao Interno da E.D.E.N
  * Funcao: Uma aplicacao de instant messages entre usuarios de uma rede.
  */
